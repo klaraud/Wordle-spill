1a21c160c54a6cde40b9f48ede65b14ef888b6e7
Klara <klarauvaagdanielsen@gmail.com>
2025-02-05 12:26:23 +0100

    Lagde liste over ord på fem bokstaver

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ for (let i = 0; i < 5*6; i++) {
     const boksElm = document.createElement("div") 
     divElm.appendChild(boksElm)
 }
+
+const ord = ["banan", "bilde", "bjørn", "blått", "brann", "bruke", "byggs", "drage", "fjell",
+    "flink", "frisk", "fugle", "gitar", "glass", "grønn", "huske", "hytte", "kasse", "klubb",
+    "krane", "lytte", "måtte", "neste", "norsk", "oljen", "pakke", "piano", "radio", "rette",
+    "sette", "skape", "skift", "skyte", "smake", "smykk", "snakk", "sovne", "spise", "stein",
+    "strek", "støtt", "svart", "sykle", "tanke", "trene", "trøtt", "tusen", "varme", "verdt",
+    "vifte", "vinter", "vokse", "vårens"]
