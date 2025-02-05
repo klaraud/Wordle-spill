bc672ddc84f2be6a3563509c629451b3634cb4d1
Klara <klarauvaagdanielsen@gmail.com>
2025-02-05 13:04:59 +0100

    Mye js oppdatering i beskrivelse
    Fikk hjelp av rasmus til å lage input-elementer i tekstboksene. Jeg fikk til at den hoppet til neste når man trykket enter

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
+const gridElm = document.querySelector(".oppsett")
 
 for (let i = 0; i < 5*6; i++) {
-    const divElm = document.querySelector(".oppsett")
     const boksElm = document.createElement("div") 
-    divElm.appendChild(boksElm)
+    gridElm.appendChild(boksElm)
 }
 
 const ord = ["banan", "bilde", "bjørn", "blått", "brann", "bruke", "byggs", "drage", "fjell",
@@ -12,3 +12,36 @@ const ord = ["banan", "bilde", "bjørn", "blått", "brann", "bruke", "byggs", "d
     "sette", "skape", "skift", "skyte", "smake", "smykk", "snakk", "sovne", "spise", "stein",
     "strek", "støtt", "svart", "sykle", "tanke", "trene", "trøtt", "tusen", "varme", "verdt",
     "vifte", "vinter", "vokse", "vårens"]
+
+
+// let guessString = ord[Math.floor(Math.random()*)]
+
+const bokser = document.querySelectorAll(".oppsett > div")
+
+for (let i = 0; i < 5; i++) {
+    const boksElm = bokser[i];
+    const inputElm = document.createElement("input")
+    inputElm.addEventListener("input", hopp)
+    boksElm.appendChild(inputElm)
+}
+
+
+function hopp(event) {
+    console.log(event)
+    const target = event.target
+    const bokstav = target.value
+
+    console.log("Du skrev:", bokstav)
+
+    console.log(target.parentElement.nextSibling.children[0])
+
+    
+    const nesteInput = target.parentElement.nextSibling?.children[0]
+    
+    target.addEventListener("keydown", function(event){
+        if (event.key === "Enter" && nesteInput){
+            nesteInput.focus()
+        }
+        
+    })
+}
