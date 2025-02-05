44a9458bdfe4c77612c2a8fb7e72d4414cc9ad3e
Klara <klarauvaagdanielsen@gmail.com>
2025-02-05 13:23:53 +0100

    Fikk til input-felt i alle boksene, startet på en funksjon for å gjette ord

--- script.js
+++ script.js
@@ -14,29 +14,31 @@ const ord = ["banan", "bilde", "bjørn", "blått", "brann", "bruke", "byggs", "d
     "vifte", "vinter", "vokse", "vårens"]
 
 
-// let guessString = ord[Math.floor(Math.random()*)]
+let guessString = ord[Math.floor(Math.random()* ord)]
 
 const bokser = document.querySelectorAll(".oppsett > div")
 
-for (let i = 0; i < 5; i++) {
-    const boksElm = bokser[i];
-    const inputElm = document.createElement("input")
-    inputElm.addEventListener("input", hopp)
-    boksElm.appendChild(inputElm)
+for (let rad = 0; rad < 6; rad++) {
+    
+    for (let i = 0; i < 5; i++) {
+        const boksElm = bokser[rad * 5 + i];
+        const inputElm = document.createElement("input")
+        inputElm.addEventListener("input", hopp)
+        boksElm.appendChild(inputElm)
+    }
 }
 
 
+//Funksjonen får den til å hoppe til input-elementet i neste rute etter man trykker enter
 function hopp(event) {
     console.log(event)
     const target = event.target
     const bokstav = target.value
 
     console.log("Du skrev:", bokstav)
-
     console.log(target.parentElement.nextSibling.children[0])
 
-    
-    const nesteInput = target.parentElement.nextSibling?.children[0]
+    const nesteInput = target.parentElement.nextSibling.children[0]
     
     target.addEventListener("keydown", function(event){
         if (event.key === "Enter" && nesteInput){
@@ -44,4 +46,10 @@ function hopp(event) {
         }
         
     })
+}
+
+function gjettOrd(){
+    let ordet = guessString
+    console.log(ordet)
+
 }
